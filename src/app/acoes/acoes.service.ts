f26e5c02dfb7587e06f1778265bd36e7ca4631a1
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-06-27 17:26:10 -0300

    chore: tap, plunk and pipe operators

--- src/app/acoes/acoes.service.ts
+++ src/app/acoes/acoes.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, pluck, tap } from 'rxjs/operators';
+import { Acao } from './modelo/acoes';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,27 @@ export class AcoesService {
   constructor(private httpCliete: HttpClient) { }
 
   getAcoes() {
-    return this.httpCliete.get<any>('http://localhost:3000/acoes');
+    return this.httpCliete
+      .get<any>('http://localhost:3000/acoes')
+      .pipe(
+        tap((valor) => console.log(valor)),
+        // map((api) => api.payload), pluck operator receive a string with the name o the property we need to extract
+        pluck('payload'),
+        map((acoes) => 
+          acoes.sort((acaoA: Acao, acaoB: Acao) => this.ordenaPorCodigo(acaoA, acaoB))
+        )
+      );
+  }
+
+  private ordenaPorCodigo(acaoA: Acao, acaoB: Acao) {
+    if(acaoA.codigo > acaoB.codigo) {
+      return 1;
+    }
+
+    if(acaoA.codigo < acaoB.codigo) {
+      return -1;
+    }
+
+    return 0;
   }
 }

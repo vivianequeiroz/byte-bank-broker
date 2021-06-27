41f8d8514c29639b1b5e6128f77718e8be87f060
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-06-27 18:43:09 -0300

    chore: using debounce time to not overload server

--- src/app/acoes/acoes.component.ts
+++ src/app/acoes/acoes.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Acoes } from './modelo/acoes';
 import { AcoesService } from './acoes.service';
-import { Subscription } from 'rxjs';
-import { switchMap, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { debounceTime, filter, startWith, switchMap, tap } from 'rxjs/operators';
 
+const ESPERA_DIGITACAO = 300;
 @Component({
   selector: 'app-acoes',
   templateUrl: './acoes.component.html',
@@ -12,13 +13,35 @@ import { switchMap, tap } from 'rxjs/operators';
 })
 export class AcoesComponent {
   acoesInput = new FormControl();
-  acoes$ = this.acoesInput.valueChanges.pipe(
-    tap(console.log),
-    switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado)), 
-    tap(console.log)
+  todasAcoes$ = this.acoesService.getAcoes().pipe(
+    tap(() => { 
+      console.log('Fluxo inicial')
+    })
   );
+
+  filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
+    debounceTime(ESPERA_DIGITACAO),
+    tap(() => { 
+      console.log('Fluxo do filtro')
+    }),
+    tap(console.log),
+    filter(
+      (valorDigitado) => valorDigitado.length >= 3 || !valorDigitado.length
+    ),
+    switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado))
+  );
+
+  acoes$: Observable<Acoes> = this.acoesInput.valueChanges
+    .pipe(
+      startWith('')
+    )
+    .pipe(
+      switchMap((valorDigitado: string) =>
+        this.acoesService.getAcoes(valorDigitado)
+      )
+    );
 
   constructor(private acoesService: AcoesService) {}
 
-
 }
+// $ in a variable's name is a coding conventions for indicating an observable
